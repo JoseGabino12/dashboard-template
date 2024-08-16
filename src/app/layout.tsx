41879f0a4007f8a653dd8aa1e7a905e84ec89e6f
JoseGabino12 <gabinoMorales1212@hotmail.com>
2024-08-16 14:45:17 -0600

    Dashboard template

---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+  description: "Dashboard template",
+  icons: {
+    icon: "/graph.svg"
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/graph.svg" type="image/svg+xml" />
+      </head>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >{children}</body>
+    </html>
+  );
+}
